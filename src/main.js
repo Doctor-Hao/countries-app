20001d7f35a7eec7c100485c42ecc559203a7456
Meng H00 <zhmuruk1@gmail.com>
2024-04-19 16:09:09 +0300

    create project and add css/fonts

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "./assets/css/global.module.scss";
 
 createApp(App).use(router).mount("#app");
